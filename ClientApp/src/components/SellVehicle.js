4fb75c6f2e62744e1045b6a860f4d39a486d8654
vandit <vandit.bo02@gmail.com>
2021-03-11 16:17:02 -0500

    Sale car UI update

--- ClientApp/src/components/SellVehicle.js
+++ ClientApp/src/components/SellVehicle.js
@@ -15,13 +15,61 @@ export class SellVehicle extends Component {
         <h1>Sell Vehicle Page!</h1>
         <br></br>
         <form onSubmit={this.submitCar}>
-          <input type="input" name="owner" placeholder="Owner" onChange={this.handleInputChange}></input>
-          <input type="input" name="year" placeholder="Year" onChange={this.handleInputChange}></input>
-          <input type="input" name="make" placeholder="Make" onChange={this.handleInputChange}></input>
-          <input type="input" name="model" placeholder="Model" onChange={this.handleInputChange}></input>
-          <input type="input" name="color" placeholder="Color" onChange={this.handleInputChange}></input>
-          <input type="input" name="price" placeholder="Price" onChange={this.handleInputChange}></input>
-          <button type="submit">Submit</button>
+<table>
+  <tr>
+    <td>
+    <label>Owner:</label>
+    </td>
+    <td>
+    <input type="input" name="owner" placeholder="Owner" onChange={this.handleInputChange}></input>
+         
+    </td>
+  </tr>
+  <tr>
+    <td>
+    <label>Year:</label>
+    </td>
+    <td>
+    <input type="input" name="year" placeholder="Year" onChange={this.handleInputChange}></input>
+         
+    </td>
+  </tr>
+  <tr>
+    <td>
+    <label>Model:</label>
+    </td>
+    <td>
+    <input type="input" name="model" placeholder="Model" onChange={this.handleInputChange}></input>
+         
+    </td>
+  </tr>
+  <tr>
+    <td>
+    <label>Color:</label>
+    </td>
+    <td>
+    <input type="input" name="color" placeholder="Color" onChange={this.handleInputChange}></input>
+         
+    </td>
+  </tr>
+   <tr>
+    <td>
+<label>Price:</label>
+    </td>
+    <td>
+    <input type="input" name="price" placeholder="Price" onChange={this.handleInputChange}></input>
+              
+    </td>
+  </tr>
+  <tr>
+    <td colSpan="2">
+    <button type="submit">Submit</button>
+    </td>
+  </tr>
+</table>
+
+
+           
         </form>
         {/* <button className="btn btn-primary" onClick={this.incrementCounter}>View History</button>&nbsp;&nbsp; */}
       </div>
